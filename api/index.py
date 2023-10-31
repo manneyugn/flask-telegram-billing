af1961ed5039be22a45a0b3074c64314f482bdda
namns <namns@vtvlive.vn>
2023-10-31 11:06:34 +0700

    test env var

--- api/index.py
+++ api/index.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+@app.route("/about")
 def about():
-    return 'About'
+    return "About"
+
+
+@app.route("/billing", methods=["GET", "POST"])
+def billing():
+    if request.method == "GET":
+        return jsonify(isError=False, message="Success", statusCode=200, data="Billing")
+    if request.method == "POST":
+        return jsonify(
+            isError=False, message="Success", statusCode=200, data= os.environ.get('TELEGRAM_KEY')
+
+        )

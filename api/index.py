e80da6f7a83d8f8c498d966116c076983e01e288
namns <namns@vtvlive.vn>
2023-11-06 15:07:18 +0700

    update api

--- api/index.py
+++ api/index.py
@@ -2,9 +2,18 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 import telegram
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 app = Flask(__name__)
 
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+SAMPLE_SPREADSHEET_ID = '1XqamczY56e232SfJv77yObfjC8sZiEet-yHUpgeHpbo'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 @app.route("/")
 def home():
@@ -22,26 +31,28 @@ async def billing():
         return jsonify(isError=False, message="Success", statusCode=200, data="Billing")
     if request.method == "POST":
         body = request.json
-        message = body.message
+        message = body["message"]
         if message is not None:
-            text = message.text
+            text = message["text"]
             if text is not None:
                 bot = telegram.Bot(os.getenv("TELEGRAM_KEY"))
-
                 token = text.split()
                 if len(token) > 0:
                     if token[0] == "/start":
-                        print("/start")
-                        await bot.send_message(chat_id=-4012657625, text="/start")
-                    elif token[0] == "/buy":
-                        print("/buy")
-                        await bot.send_message(chat_id=-4012657625, text="/buy")
-                    elif token[0] == "/link":
-                        print("/link")
-                        await bot.send_message(chat_id=-4012657625, text="/link")
-                    else:
-                        print("Câu lệnh chưa được hỗ trợ")
                         await bot.send_message(
-                            chat_id=-4012657625, text="Câu lệnh chưa được hỗ trợ"
+                            chat_id=os.getenv("CHAT_ID"), text="/start"
+                        )
+                    elif token[0] == "/buy":
+                        await bot.send_message(
+                            chat_id=os.getenv("CHAT_ID"), text="/buy"
+                        )
+                    elif token[0] == "/link":
+                        await bot.send_message(
+                            chat_id=os.getenv("CHAT_ID"), text="/link"
+                        )
+                    else:
+                        await bot.send_message(
+                            chat_id=os.getenv("CHAT_ID"),
+                            text="Câu lệnh chưa được hỗ trợ",
                         )
         return jsonify(isError=False, message="Success", statusCode=200, data="POST")
